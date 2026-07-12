06d4581c461062ce610bd6898428b8de2c3b7972
LordEdward98 <edwar.grll98@gmail.com>
2026-07-12 14:42:12 -0600

    finalizado

--- Program.cs
+++ Program.cs
@@ -24,23 +24,40 @@ class Program
         Console.WriteLine("ingrese la cantidad de materias inscritas");
         int cantidadMaterias = Convert.ToInt32(Console.ReadLine()!);
 
-
-        if (cantidadMaterias <= materiaMaxima)
-        {
-            
-        }
         Console.WriteLine("================================");
         Console.WriteLine("Bienvenido al sistema de registro de estudiantes");
         Console.WriteLine("================================");
         Console.WriteLine("Bienvenido/a " + nombre + " " + apellido);
+        Console.WriteLine("");
         Console.WriteLine("Nombre: " + nombre);
         Console.WriteLine("Apellido: " + apellido);
         Console.WriteLine("Edad: " + edad);
+        Console.WriteLine("edad dentro de 5 años: " + (edad + 5));
+        Console.WriteLine("");
         Console.WriteLine("Carrera: " + carrera);
         Console.WriteLine("Carnet: " + carnet);
         Console.WriteLine("Correo electrónico: " + correo);
         Console.WriteLine("Teléfono: " + telefono);
         Console.WriteLine("Promedio: " + promedio);
-        Console.WriteLine("Materias inscritas: " + materias);
+        Console.WriteLine("");
+        Console.WriteLine("Materias inscritas: " + cantidadMaterias);
+        Console.WriteLine("Materia máxima permitida: " + materiaMaxima);
+        Console.WriteLine("");
+        if (cantidadMaterias < materiaMaxima)
+        {
+            Console.WriteLine("puede inscribir mas materias: si");
+        }
+        else if (cantidadMaterias == materiaMaxima)
+        {
+            Console.WriteLine("puede inscribir mas materias: no");
+        }
+        else
+        {
+            Console.WriteLine("no puede inscribir mas de 6 materias");
+        }
+        Console.WriteLine("");
+        Console.WriteLine("================================");
+        Console.WriteLine("gracias por utulizar el sistema");
+        Console.WriteLine("================================");
     }
 }
